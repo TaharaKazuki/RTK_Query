8831666bf7aac2453fb4e8420454665101871694
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-11-19 21:12:11 +0900

    update

--- lesson_02/src/features/posts/postsSlice.tsx
+++ lesson_02/src/features/posts/postsSlice.tsx
@@ -8,6 +8,28 @@ type PostState = {
   content: string
   userId: string
   date: string
+  reactions: Reaction
+}
+
+type Reaction = {
+  thumbsUp: number
+  wow: number
+  heart: number
+  rocket: number
+  coffee: number
+}
+
+const initialReactions = {
+  thumbsUp: 0,
+  wow: 0,
+  heart: 0,
+  rocket: 0,
+  coffee: 0,
+}
+
+type ReactionPayload = {
+  postId: string
+  reaction: keyof typeof initialReactions
 }
 
 const initialState: PostState[] = [
@@ -17,6 +39,7 @@ const initialState: PostState[] = [
     content: 'good things',
     userId: '0',
     date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: { ...initialReactions },
   },
   {
     id: nanoid(),
@@ -24,6 +47,7 @@ const initialState: PostState[] = [
     content: 'pizza',
     userId: '0',
     date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: { ...initialReactions },
   },
 ]
 
@@ -43,9 +67,17 @@ export const postsSlice = createSlice({
             content,
             userId,
             date: new Date().toISOString(),
+            reactions: { ...initialReactions },
           },
         }
       },
+    },
+    reactionAdded: (state, action: PayloadAction<ReactionPayload>) => {
+      const { postId, reaction } = action.payload
+      const existingPost = state.find((post) => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
+      }
     },
   },
 })
