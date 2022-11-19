fda51d7cc8b91c9f7b2d957cbe3fe0c609e89da0
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-11-19 20:50:32 +0900

    update

--- lesson_02/src/components/PostList.tsx
+++ lesson_02/src/components/PostList.tsx
@@ -1,17 +1,20 @@
 import { useAppSelector } from '../app/hooks'
 import { selectAllPosts } from '../features/posts/postsSlice'
 import PostAuthor from './PostAuthor'
+import TimeAgo from './TimeAgo'
 
 const PostList = () => {
   const posts = useAppSelector(selectAllPosts)
+  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
-  const renderedPosts = posts.map((post) => (
+  const renderedPosts = orderedPosts.map((post) => (
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
 
       <p className="postCredit">
         <PostAuthor userId={post.userId} />
+        <TimeAgo timeStamp={post.date} />
       </p>
     </article>
   ))
