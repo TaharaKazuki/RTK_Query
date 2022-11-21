dbcd6d04b5090c60d8543e9aacd2169a080bf8a5
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-11-21 11:30:55 +0900

    update

--- styled-component-training/src/components/App.tsx
+++ styled-component-training/src/components/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Button } from 'components/common/Button'
 import { createGlobalStyle } from 'styled-components'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from 'components/pages/Home'
+import Login from 'components/pages/Login'
 
 const GlobalStyle = createGlobalStyle`
   body{
@@ -16,12 +19,16 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <h1>App</h1>
-      <Button>Primary Button</Button>
-      <Button secondary large>
-        Secondary Button
-      </Button>
-      <Button disabled>Disabled Button</Button>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </>
   )
 }
